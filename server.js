0dad20652d794050f90f825c4062210109542cb7
Himanshu Garg <hg631124@gmail.com>
2023-04-03 18:32:33 +0530

    Chat gpt application server started

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const morgan=require('morgan')
+const cors=require('cors')
+const bodyParser=require('body-parser');
+const colors=require('colors');
+const dotenv=require('dotenv');
+const connectDB = require('./config/db');
+//rest object
+const app=express()
+//dotenv
+dotenv.config()
+connectDB();
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(morgan('dev'))
+
+const PORT=process.env.PORT||8080
+
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${process.env.DEV_MODE} on ${PORT}`)
+})
